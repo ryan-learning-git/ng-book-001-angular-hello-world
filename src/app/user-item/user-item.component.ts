746d4fbcf1897cbc4db915afa189f56c00726239
Ryan Williams <ryan.williams@projectpeople.com>
2017-11-29 13:13:52 +0000

    Completed up to page 50

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.css']
+})
+export class UserItemComponent implements OnInit {
+
+  @Input() name: string;
+
+  constructor() {
+    //this.name = 'Ryan';
+  }
+
+  ngOnInit() {
+  }
+
+}
